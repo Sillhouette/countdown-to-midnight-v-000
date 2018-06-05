5dfeed7fb080321d2c4d3d62f8960d92f4bf85ce
Richard Melchior <raustin.melchior@gmail.com>
2018-06-05 00:17:01 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -12,11 +12,11 @@ end
 ##
 # #countdown_with_sleep counts down from the input until 0 with one second between each number then returns "HAPPY NEW YEAR!"
 ##
-def countdown_with_sleep(sleep)
-  while number > 0
-  puts "#{number} SECOND(S)!"
-  sleep(1)
-  number -= 1
-end
-"HAPPY NEW YEAR!"
+def countdown_with_sleep(number)
+  while (number > 0) do
+    puts "#{number} SECONDS!";
+    sleep(1);
+    number -= 1;
+  end
+  "HAPPY NEW YEAR!";
 end
